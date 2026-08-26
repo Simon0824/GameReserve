d3591cc88e9d68d353092115f05f9c7a223394ff
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-26 10:47:53 +0200

    fix: add .json cofiguration files to .gitignore

--- src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
+++ src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3672134b9081bce0f35fa09af91ea2762fbd7ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e516ba3ed7f11307af5e4147f4e13952ecd70ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
