57ea618a818ccdee8e841f1753e4b96f95b30505
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-27 19:39:36 +0200

    fix: specify User type in IdentityDbContext

--- src/Modules/Identity/Identity.Infrastructure/Data/IdentityContext.cs
+++ src/Modules/Identity/Identity.Infrastructure/Data/IdentityContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Infrastructure.Data;
-public class IdentityContext : IdentityDbContext
+public class IdentityContext : IdentityDbContext<User>
 {
     public IdentityContext(DbContextOptions<IdentityContext> options) : base(options){}
 
