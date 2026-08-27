19159625150bcf21e1db8ea87580e48082e334ac
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-27 10:31:32 +0200

    feat: add a create domain method in user aggregate

--- src/Modules/Identity/Identity.Domain/UserAggregate/User.cs
+++ src/Modules/Identity/Identity.Domain/UserAggregate/User.cs
@@ -6,4 +6,16 @@ public class User
     public string Email {get; private set;} = string.Empty;
     public string Password {get; private set;} = string.Empty;
 
+    public static User Create(Guid id, string fullname, string email, string password)
+    {
+        var user = new User()
+        {
+            Id = id,
+            FullName = fullname,
+            Email = email,
+            Password = password
+        };
+
+        return user;
+    }
 }
