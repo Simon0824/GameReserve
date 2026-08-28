ace2d57dd6077868d5c45f512939c9b6487b3103
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-28 19:13:16 +0200

    feat: add domain events to Entity and make it abstract

--- src/Modules/Identity/Identity.Domain/Abstractions/Entity.cs
+++ src/Modules/Identity/Identity.Domain/Abstractions/Entity.cs
@@ -1,7 +1,9 @@
+using Identity.Domain.Primitives;
 using Microsoft.AspNetCore.Identity;
 
 namespace Identity.Domain.Abstractions;
-public class Entity : IdentityUser
+public abstract class Entity : IdentityUser
 {
-    
+    private List<DomainEvent> _domainEvents = new();
+    public IReadOnlyList<DomainEvent> DomainEvents => _domainEvents;
 }
