42499c67978d020f3445f9020e25e59b45c95c6d
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-28 19:34:17 +0200

    feat: add Status property to User aggregate

--- src/Modules/Identity/Identity.Domain/UserAggregate/User.cs
+++ src/Modules/Identity/Identity.Domain/UserAggregate/User.cs
@@ -1,15 +1,18 @@
 using Identity.Domain.Abstractions;
+using Identity.Domain.Enums;
 
 namespace Identity.Domain.UserAggregate;
 public class User : Entity
 {
     public string FullName {get; private set;} = string.Empty;
+    public UserStatus Status {get; private set;}
 
     public static User Create(string fullName, string email)
     {
         var user = new User()
         {
             FullName = fullName,
+            Status = UserStatus.Active,
             Email = email
         };
 
