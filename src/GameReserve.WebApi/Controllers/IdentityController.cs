7ef3c34a8abaab68713361e1871472f2c0ed58ec
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-29 12:35:09 +0200

    refactor: use repository in user creation endpoint

--- src/GameReserve.WebApi/Controllers/IdentityController.cs
+++ src/GameReserve.WebApi/Controllers/IdentityController.cs
@@ -12,6 +12,12 @@ public class IdentityController(UserRepository userRepository) : ControllerBase
     public async Task<IActionResult> CreateUser([FromBody] UserCreateDTO dto)
     {
          var user = Identity.Domain.UserAggregate.User.Create(dto.FullName, dto.Email);
+         var result = await userRepository.CreateUser(user, dto.Password);
+         if(!result.Succeeded)
+         {
+            throw new Exception("Cannot create a user");
+         }
+
          return Ok(user);
     }
 }
