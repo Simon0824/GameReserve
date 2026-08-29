560060a95e2094a0e593c29ae40c15cd73fc4ab6
Szymon Szostek <szymon.szostek008@gmail.com>
2026-08-29 12:13:52 +0200

    feat: add CreateUser method in UserRepo

--- src/Modules/Identity/Identity.Domain/Interfaces/IUserRepository.cs
+++ src/Modules/Identity/Identity.Domain/Interfaces/IUserRepository.cs
@@ -1,5 +1,8 @@
+using Identity.Domain.UserAggregate;
+using Microsoft.AspNetCore.Identity;
+
 namespace Identity.Domain.Interfaces;
 public interface IUserRepository
 {
-    
+    Task<IdentityResult> CreateUser(User user, string password);
 }
--- src/Modules/Identity/Identity.Infrastructure/Repositories/UserRepository.cs
+++ src/Modules/Identity/Identity.Infrastructure/Repositories/UserRepository.cs
@@ -1,7 +1,12 @@
 using Identity.Domain.Interfaces;
+using Identity.Domain.UserAggregate;
+using Microsoft.AspNetCore.Identity;
 
 namespace Identity.Infrastructure.Repositories;
-public class UserRepository() : IUserRepository
+public class UserRepository(UserManager<User> userManager) : IUserRepository
 {
-    
+    public async Task<IdentityResult> CreateUser(User user, string password)
+    {
+        return await userManager.CreateAsync(user, password);
+    }
 }
