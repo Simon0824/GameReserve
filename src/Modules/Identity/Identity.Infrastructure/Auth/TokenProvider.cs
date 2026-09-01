8b82c4e660bc30b0ae85f9c900f0e295660f2b4a
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-01 18:06:03 +0200

    feat: add security key and create Jwt token

--- src/Modules/Identity/Identity.Infrastructure/Auth/TokenProvider.cs
+++ src/Modules/Identity/Identity.Infrastructure/Auth/TokenProvider.cs
@@ -1,5 +1,37 @@
+using System.Security.Claims;
+using System.Text;
+using Identity.Domain.UserAggregate;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+
 namespace Identity.Infrastructure.Auth;
 
-public class TokenProvider
+public class TokenProvider(IConfiguration configuration)
 {
+    public string CreateToken(User user)
+    {
+        var secretKey = configuration["Jwt:SecretKey"];
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!));
+
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var descriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity([
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email!)
+            ]),
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddMinutes(5),
+            Issuer = configuration["Jwt:Issuer"],
+            Audience = configuration["Jwt:Audience"]
+        };
+
+        var handler = new JsonWebTokenHandler();
+        var token = handler.CreateToken(descriptor);
+
+        return token;
+    }
 }
