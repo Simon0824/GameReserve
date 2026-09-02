6a1d9e946890cb66d212a878895bf5d26f7514c1
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-02 17:05:21 +0200

    feat: add AddUserRole to UserRepository interface

--- src/Modules/Identity/Identity.Domain/Interfaces/IUserRepository.cs
+++ src/Modules/Identity/Identity.Domain/Interfaces/IUserRepository.cs
@@ -7,4 +7,5 @@ public interface IUserRepository
     Task<IdentityResult> CreateUser(User user, string password);
     Task<User?> FindUser(string email);
     Task<bool> CheckPassword(User user, string password);
+    Task<IdentityResult> AddUserRole(User user);
 }
