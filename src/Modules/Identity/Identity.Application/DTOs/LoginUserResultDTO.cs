6622a7c56fa22658b32f764d958c4122a7248db9
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-03 16:29:16 +0200

    feat: add RefreshToken property to LoginUser result DTO

--- src/Modules/Identity/Identity.Application/DTOs/LoginUserResultDTO.cs
+++ src/Modules/Identity/Identity.Application/DTOs/LoginUserResultDTO.cs
@@ -1,2 +1,4 @@
+using Identity.Domain.Entites;
+
 namespace Identity.Application.DTOs;
-public record LoginUserResultDTO(string FullName, string Email, string Token);
+public record LoginUserResultDTO(string FullName, string Email, string Token, string RefreshToken);
