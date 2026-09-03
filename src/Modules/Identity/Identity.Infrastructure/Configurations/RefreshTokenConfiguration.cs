b9e5086d5fb862a4861947488dbd3eb4ccc3fffe
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-03 11:17:57 +0200

    feat: add EF Core configurations to refresh token

--- src/Modules/Identity/Identity.Infrastructure/Configurations/RefreshTokenConfiguration.cs
+++ src/Modules/Identity/Identity.Infrastructure/Configurations/RefreshTokenConfiguration.cs
@@ -1,4 +1,5 @@
 using Identity.Domain.Entites;
+using Identity.Domain.UserAggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,5 +9,10 @@ public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
     public void Configure(EntityTypeBuilder<RefreshToken> builder)
     {
         builder.HasKey(k => k.Id);
+
+        builder.Property(p => p.Token).HasMaxLength(200);
+        builder.HasIndex(p => p.Token).IsUnique();
+
+        builder.HasOne(p => p.User).WithMany().HasForeignKey(p => p.UserId);
     }
 }
