e36c8cfc2018626ed67907a78a33cc0a7970a1db
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-04 18:21:23 +0200

    feat(auth): implement access and refresh token rotation

--- src/Modules/Identity/Identity.Application/Commands/LoginWithRefreshTokenCommand.cs
+++ src/Modules/Identity/Identity.Application/Commands/LoginWithRefreshTokenCommand.cs
@@ -5,9 +5,23 @@ using MediatR;
 namespace Identity.Application.Commands;
 public record LoginWithRefreshTokenCommand(string RefreshToken) : IRequest<LoginWithRefreshTokenResultDTO>;
 
-public class LoginWithRefreshTokenCommandHandler(IUserRepository userRepository) : IRequestHandler<LoginWithRefreshTokenCommand, LoginWithRefreshTokenResultDTO>
+public class LoginWithRefreshTokenCommandHandler(IUserRepository userRepository, ITokenProvider tokenProvider) : IRequestHandler<LoginWithRefreshTokenCommand, LoginWithRefreshTokenResultDTO>
 {
     public async Task<LoginWithRefreshTokenResultDTO> Handle(LoginWithRefreshTokenCommand request, CancellationToken cancellationToken)
     {
+        var refreshToken = await userRepository.FindRefreshToken(request.RefreshToken);
+        if(refreshToken is null || refreshToken.ExpiresOnUtc < DateTime.UtcNow)
+        {
+            throw new Exception("Refresh token is not foud or has expired");
+        }
+
+        var accessToken = await tokenProvider.CreateToken(refreshToken.User);
+
+        refreshToken.Token = tokenProvider.GenerateRefreshToken();
+
+        return new LoginWithRefreshTokenResultDTO(
+            accessToken,
+            refreshToken.Token
+        );
     }
 }
