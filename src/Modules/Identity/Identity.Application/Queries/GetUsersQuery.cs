c31f51020304525ccff1ec3e49296c2d743f326e
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-05 15:09:35 +0200

    feat(cqrs): implement GetUsersQuery returning user list DTO

--- src/Modules/Identity/Identity.Application/Queries/GetUsersQuery.cs
+++ src/Modules/Identity/Identity.Application/Queries/GetUsersQuery.cs
@@ -1,4 +1,29 @@
+using Identity.Application.DTOs;
+using Identity.Domain.Interfaces;
 using MediatR;
 
 namespace Identity.Application.Queries;
-public record GetUsersQuery() : IRequest;
+public record GetUsersQuery : IRequest<List<GetUsersResultDTO>>;
+public class GetUsersQueryHandler(IUserRepository userRepository) : IRequestHandler<GetUsersQuery, List<GetUsersResultDTO>>
+{
+    public async Task<List<GetUsersResultDTO>> Handle(GetUsersQuery request, CancellationToken token)
+    {
+        var users = await userRepository.GetUsers();
+        var result = new List<GetUsersResultDTO>();
+        if(users is null)
+        {
+            throw new Exception("There is no users in database");
+        }
+
+        foreach(var user in users)
+        {
+            result.Add(new GetUsersResultDTO(
+                user.Id,
+                user.FullName,
+                user.Email!
+            ));
+        }
+
+        return result;
+    }
+}
