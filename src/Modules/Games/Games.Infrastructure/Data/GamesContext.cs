e7a3a02432beee67b871cde8295bfcfbfea1d43e
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-06 12:33:59 +0200

    feat(games): add DbContext in Infrastructure layer, configure GamesContext

--- src/Modules/Games/Games.Infrastructure/Data/GamesContext.cs
+++ src/Modules/Games/Games.Infrastructure/Data/GamesContext.cs
@@ -7,4 +7,10 @@ public class GamesContext : DbContext
     public GamesContext(DbContextOptions<GamesContext> options) : base(options) {}
 
     public DbSet<Game> games {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(GamesContext).Assembly);
+    }
 }
--- src/Modules/Games/Games.Infrastructure/DependencyInjection.cs
+++ src/Modules/Games/Games.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Games.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Games.Infrastructure;
@@ -7,6 +9,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddGamesInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<GamesContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
         return services;
     }
 }
