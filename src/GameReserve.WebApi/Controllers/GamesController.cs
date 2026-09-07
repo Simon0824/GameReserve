14d6b2e1e98ab4a6484ad37408c79e890718d573
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-07 16:19:12 +0200

    feat(games): create AddGame endpoint

--- src/GameReserve.WebApi/Controllers/GamesController.cs
+++ src/GameReserve.WebApi/Controllers/GamesController.cs
@@ -1,9 +1,17 @@
+using Games.Application.Commands;
+using Games.Application.DTOs;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameReserve.WebApi.Controllers;
 [ApiController]
 [Route("api/[controller]")]
-public class GamesController : ControllerBase
+public class GamesController(ISender sender) : ControllerBase
 {
-    
+    [HttpPost("add-game")]
+    public async Task<IActionResult> AddNewGame([FromBody] AddNewGameDTO dto)
+    {
+        var resultDTO = await sender.Send(new AddNewGameCommand(dto.Title, dto.Description));
+        return Ok(resultDTO);
+    }
 }
--- src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
+++ src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c56f76933e07c9ab842dd80f954630421d3cfdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a123c40f0043982e94e2470621780b1f9e9bd67a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
