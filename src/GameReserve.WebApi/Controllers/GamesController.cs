f7e92528865803167203abc4471eef48834b0d71
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-07 16:31:52 +0200

    feat(games): add Admin authorization to AddGame endpoint

--- src/GameReserve.WebApi/Controllers/GamesController.cs
+++ src/GameReserve.WebApi/Controllers/GamesController.cs
@@ -1,14 +1,18 @@
 using Games.Application.Commands;
 using Games.Application.DTOs;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Domain.Constants;
 
 namespace GameReserve.WebApi.Controllers;
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class GamesController(ISender sender) : ControllerBase
 {
     [HttpPost("add-game")]
+    [Authorize(Roles = UserRoles.Admin)]
     public async Task<IActionResult> AddNewGame([FromBody] AddNewGameDTO dto)
     {
         var resultDTO = await sender.Send(new AddNewGameCommand(dto.Title, dto.Description));
