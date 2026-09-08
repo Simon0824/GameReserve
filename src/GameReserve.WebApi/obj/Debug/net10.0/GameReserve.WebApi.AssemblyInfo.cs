639a094930c87664c40aa3ea3ae811ed8eaa0be7
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-08 17:22:37 +0200

    feat(games): add GetGamesCatalogResultDto

--- src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
+++ src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e92528865803167203abc4471eef48834b0d71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d04280caf259f19b6ef2a5ae5d7088ec01b59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Modules/Games/Games.Application/DTOs/GetGamesCatalogResultDTO.cs
@@ -0,0 +1,2 @@
+namespace Games.Application.DTOs;
+public record GetGamesCatalogResultDTO(string Id, string Title, string Description);
