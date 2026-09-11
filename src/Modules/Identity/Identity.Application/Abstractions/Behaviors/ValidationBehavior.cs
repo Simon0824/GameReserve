b40fd9c7af7ec288240d61e0da6ca861dfe9a7ef
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-11 21:03:10 +0200

    feat(cqrs): implement validation pipeline behavior

--- src/Modules/Identity/Identity.Application/Abstractions/Behaviors/ValidationBehavior.cs
+++ src/Modules/Identity/Identity.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -12,6 +12,27 @@ public class ValidationBehavior<TRequest, TResponse>(
         RequestHandlerDelegate<TResponse> next, 
         CancellationToken cancellationToken)
     {
+        if(!validators.Any())
+        {
+            return await next();
+        }
+        
+        var context = new ValidationContext<TRequest>(request);
+
+        var results = await Task.WhenAll(
+            validators.Select(
+                v => v.ValidateAsync(context, cancellationToken)
+                ));
+
+        var failures = results.SelectMany(r => r.Errors)
+                              .Where(f => f is not null)
+                              .ToList();
+        
+        if(failures.Count > 0)
+        {
+            throw new ValidationException(failures);
+        }
+
         return await next();
     }
 }
