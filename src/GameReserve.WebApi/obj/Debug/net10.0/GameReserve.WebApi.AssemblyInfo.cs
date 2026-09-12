f08c54eb8bfc33c44fb28b27424b8de782b46cc1
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-12 08:51:34 +0200

    feat(db): add event publishing interceptor

--- src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
+++ src/GameReserve.WebApi/obj/Debug/net10.0/GameReserve.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40fd9c7af7ec288240d61e0da6ca861dfe9a7ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1361c219d073b31ab5875cee9a9bfc2af516e6cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameReserve.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Modules/Identity/Identity.Infrastructure/Data/IdentityContext.cs
+++ src/Modules/Identity/Identity.Infrastructure/Data/IdentityContext.cs
@@ -1,7 +1,11 @@
+using Identity.Domain.Abstractions;
 using Identity.Domain.Entities;
 using Identity.Domain.UserAggregate;
+using MediatR;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Identity.Infrastructure.Data;
 public class IdentityContext : IdentityDbContext<User>
@@ -15,5 +19,38 @@ public class IdentityContext : IdentityDbContext<User>
     {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(IdentityContext).Assembly);
+    }
+}
+
+public class PublishDomainEventsInterceptor(IPublisher publisher) : SaveChangesInterceptor
+{
+    public override async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = default)
+    {
+        if(eventData.Context is not null)
+        {
+            await PublishDomainEvents(eventData.Context);
+        }
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task PublishDomainEvents(DbContext context)
+    {
+        var entities = context.ChangeTracker
+                                    .Entries<Entity>()
+                                    .Select(entry => entry.Entity)
+                                    .Where(entity => entity.DomainEvents.Count > 0)
+                                    .ToList();
+
+        var domainEvents = entities.SelectMany(entity => entity.DomainEvents).ToList();
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await publisher.Publish(domainEvent);
+        }
+
+        foreach(var entity in entities)
+        {
+            entity.ClearDomainEvents();
+        }
     }
 }
