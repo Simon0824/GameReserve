16dcfb3a6198e7e7fff67f69cf4eaeea83082556
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-14 15:47:07 +0200

    feat(reservations): add ReservationProfile with properties

--- src/Modules/Reservations/Reservations.Domain/Aggregates/ReservationProfile.cs
+++ src/Modules/Reservations/Reservations.Domain/Aggregates/ReservationProfile.cs
@@ -1,4 +1,12 @@
+using Reservations.Domain.Primitives;
 using SharedKernel.Domain.Abstractions;
 
 namespace Reservations.Domain.Aggregates;
-public class ReservationProfile{}
+public class ReservationProfile : Entity<ReservationProfileId>
+{
+    public Guid UserId {get; private set;}
+    private List<Reservation> _reservations {get; set;} = new ();
+    public IReadOnlyList<Reservation> reservations => _reservations;
+
+    private ReservationProfile() {}
+}
