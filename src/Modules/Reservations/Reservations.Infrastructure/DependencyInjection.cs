6c8bf5a720a5ba4b120b29d39261c4139c3c7ef8
Szymon Szostek <szymon.szostek008@gmail.com>
2026-09-14 16:45:02 +0200

    feat(reservations): register ReservationsContext in reservations DI

--- src/Modules/Reservations/Reservations.Infrastructure/DependencyInjection.cs
+++ src/Modules/Reservations/Reservations.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Reservations.Infrastructure.Data;
 
 namespace Reservations.Infrastructure;
 
@@ -7,6 +9,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddReservationsInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ReservationsContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
         return services;
     }
 }
