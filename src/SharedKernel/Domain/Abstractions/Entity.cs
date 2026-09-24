18a9616e493fe6b43209cb0b1442c6165626da5e
Simon0824 <szymon.szostek008@gmail.com>
2026-09-24 11:18:05 +0200

    feat(shared-kernel): add domain events list and raise,clear methods to entity

--- src/SharedKernel/Domain/Abstractions/Entity.cs
+++ src/SharedKernel/Domain/Abstractions/Entity.cs
@@ -1,12 +1,25 @@
+using SharedKernel.Domain.Primivites.DomainEvent;
+
 namespace SharedKernel.Domain.Abstractions;
 public abstract class Entity<TId> where TId : notnull
 {
     public TId Id {get;  set;} = default!;
 
+    protected List<DomainEvent> domainEvents = new();
+
+    public IReadOnlyList<DomainEvent> DomainEvents => domainEvents;
+
     protected Entity(TId id)
     {
         Id = id;
     }
+
+    public void Raise(DomainEvent domainEvent)
+    {
+        domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents() => domainEvents.Clear();
 
     protected Entity()
     {}
